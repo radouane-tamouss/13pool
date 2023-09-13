b4c4593b8d3ce9e4d00da2386e26da33eb48da4b
Radouane Tamouss <rtamouss@e1r2p9.1337.ma>
2023-09-13 20:17:54 +0100

    c00

---
+++ c00/ex05/ft_print_comb.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rtamouss <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/13 15:46:17 by rtamouss          #+#    #+#             */
+/*   Updated: 2023/09/13 18:17:03 by rtamouss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char i, char j, char k)
+{
+	write(1, &i, 1);
+	write(1, &j, 1);
+	write(1, &k, 1);
+}
+
+void	ft_print_comb(void)
+{
+	char	i;
+	char	j;
+	char	k;
+
+	i = '0';
+	while (i <= '7')
+	{
+		j = i + 1;
+		while (j <= '8')
+		{
+			k = j + 1;
+			while (k <= '9')
+			{
+				ft_putchar(i, j, k);
+				if (i != '7' || j != '8' || k != '9')
+					write(1, ", ", 2);
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+	write(1, "\n", 1);
+}
+
+int	main(void)
+{
+	ft_print_comb();
+	return (0);
+}
