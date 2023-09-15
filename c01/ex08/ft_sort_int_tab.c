8644f0922fc1bbf80e47ed0a0109b28489d93923
Radouane Tamouss <rtamouss@e1r3p11.1337.ma>
2023-09-16 00:54:19 +0100

    finally

--- c01/ex08/ft_sort_int_tab.c
+++ c01/ex08/ft_sort_int_tab.c
@@ -6,36 +6,28 @@
 /*   By: rtamouss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 21:26:56 by rtamouss          #+#    #+#             */
-/*   Updated: 2023/09/16 00:29:09 by rtamouss         ###   ########.fr       */
+/*   Updated: 2023/09/16 00:54:08 by rtamouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_swap(int a, int b)
-{
-	int	swap;
-
-	swap = a;
-	a = b;
-	b = swap;
-}
-
 void	ft_sort_int_tab(int *tab, int size)
 {
 	int	i;
+	int	swap;
 
 	i = 0;
-	while (i < size)
+	while (i < size - 1)
 	{
 		if (tab[i] > tab[i + 1])
 		{
-			ft_swap(tab[i], tab[i + 1]);
+			swap = tab[i];
+			tab[i] = tab[i + 1];
+			tab[i + 1] = swap;
 			i = 0;
 		}
 		else
-		{
 			i++;
-		}
 	}
 }
