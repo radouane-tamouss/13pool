723f4eeb88080071128e6459a153d6ea8ddcdbe0
Radouane Tamouss <rtamouss@e1r3p11.1337.ma>
2023-09-16 00:29:28 +0100

    hello ;

--- c01/ex08/ft_sort_int_tab.c
+++ c01/ex08/ft_sort_int_tab.c
@@ -6,7 +6,7 @@
 /*   By: rtamouss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 21:26:56 by rtamouss          #+#    #+#             */
-/*   Updated: 2023/09/16 00:23:22 by rtamouss         ###   ########.fr       */
+/*   Updated: 2023/09/16 00:29:09 by rtamouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,16 @@ void	ft_sort_int_tab(int *tab, int size)
 	int	i;
 
 	i = 0;
-	while (i < size - 1)
+	while (i < size)
 	{
 		if (tab[i] > tab[i + 1])
 		{
 			ft_swap(tab[i], tab[i + 1]);
+			i = 0;
 		}
-		i++;
+		else
+		{
+			i++;
+		}
 	}
 }
