66fa492661ce8ffb7952c9be1bf01d550c6c1838
Radouane Tamouss <rtamouss@e1r3p11.1337.ma>
2023-09-15 23:49:25 +0100

    hello

--- c01/ex07/ft_rev_int_tab.c
+++ c01/ex07/ft_rev_int_tab.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rtamouss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/15 20:45:01 by rtamouss          #+#    #+#             */
-/*   Updated: 2023/09/15 22:03:21 by rtamouss         ###   ########.fr       */
+/*   Created: 2023/09/15 23:48:41 by rtamouss          #+#    #+#             */
+/*   Updated: 2023/09/15 23:49:04 by rtamouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	ft_rev_int_tab(int *tab, int size)
 	int	i;
 
 	i = 0;
-	while (i <= size / 2)
+	while (i <= (size / 2))
 	{
 		ft_swap(&tab[i], &tab[size - i - 1]);
 		i++;
