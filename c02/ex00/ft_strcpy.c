f1aef5145a8a1fcb1dd134e5483f128fa5c4db37
Radouane Tamouss <rtamouss@e2r3p2.1337.ma>
2023-09-16 18:09:57 +0100

    hel

--- c02/ex00/ft_strcpy.c
+++ c02/ex00/ft_strcpy.c
@@ -6,30 +6,22 @@
 /*   By: rtamouss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/16 14:40:23 by rtamouss          #+#    #+#             */
-/*   Updated: 2023/09/16 15:51:32 by rtamouss         ###   ########.fr       */
+/*   Updated: 2023/09/16 16:34:23 by rtamouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdio.h>
 
 char	*ft_strcpy(char *dest, char *src)
 {
 	int	i;
 
 	i = 0;
-	while(src[i])
+	while (src[i])
 	{
 		dest[i] = src[i];
 		i++;
 	}
 	dest[i] = '\0';
-	return *dest;
+	return (dest);
 }
-
-int  main(void)
-{
-	char	dest[] = "hello";
-	char	src[] = "kklls";
-
-	printf("%s , %s \n", dest[]
