551f4b4d7b41238385f00625b047f5c46fb15d10
Radouane Tamouss <rtamouss@e2r9p11.1337.ma>
2023-09-18 22:00:29 +0100

    helo

--- c02/ex10/ft_strlcpy.c
+++ c02/ex10/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: rtamouss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 15:24:34 by rtamouss          #+#    #+#             */
-/*   Updated: 2023/09/18 21:58:04 by rtamouss         ###   ########.fr       */
+/*   Updated: 2023/09/18 22:00:21 by rtamouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,8 @@ int	ft_strlen(char *str)
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
 	unsigned int	i;
-	unsigned int	src_len;
 
 	i = 0;
-	src_len = ft_strlen(src);
 	if (src[0] != '\0')
 	{
 		while (src[i] != '\0' && i < size - 1)
