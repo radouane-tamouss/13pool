3e555812e218758c8e24dcbb91c4edf9daa64f6c
Radouane Tamouss <rtamouss@e2r9p11.1337.ma>
2023-09-18 19:54:23 +0100

    hello

--- c02/ex01/ft_strncpy.c
+++ c02/ex01/ft_strncpy.c
@@ -6,18 +6,19 @@
 /*   By: rtamouss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 13:33:10 by rtamouss          #+#    #+#             */
-/*   Updated: 2023/09/18 18:51:17 by rtamouss         ###   ########.fr       */
+/*   Updated: 2023/09/18 19:00:13 by rtamouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int	i;
 
 	i = 0;
-	while (i < 0 && src[i] != '\0')
+	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
@@ -29,3 +30,13 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	}
 	return (dest);
 }	
+
+int	main(void)
+{
+	char dest[] = "Hello World!!";
+	char src[] = "Hello World Thiss begig";
+
+	printf("%s", ft_strncpy(dest, src, 6));
+	return 0;
+
+}
