57ea5de91706c13fd077e246a47c26fa7a88e49c
Radouane Tamouss <rtamouss@e2r12p2.1337.ma>
2023-09-20 16:17:48 +0100

    hello

--- c03/ex05/ft_strlcat.c
+++ c03/ex05/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: rtamouss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 15:54:29 by rtamouss          #+#    #+#             */
-/*   Updated: 2023/09/20 16:13:07 by rtamouss         ###   ########.fr       */
+/*   Updated: 2023/09/20 16:17:32 by rtamouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,8 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 
 	dstlen = ft_strlen(dest);
 	i = 0;
+	if (size == 0 || dstlen >= size)
+		return (ft_strlen(src));
 	while (src[i] && i < size - dstlen - 1)
 	{
 		dest[dstlen + i] = src[i];
