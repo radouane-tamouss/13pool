30c5fbc73e780d318f22f3f1a2849a405f96493e
Radouane Tamouss <rtamouss@e2r12p2.1337.ma>
2023-09-20 15:08:55 +0100

    hel

--- c03/ex04/ft_strstr.c
+++ c03/ex04/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: rtamouss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 13:23:31 by rtamouss          #+#    #+#             */
-/*   Updated: 2023/09/20 14:10:22 by rtamouss         ###   ########.fr       */
+/*   Updated: 2023/09/20 15:08:45 by rtamouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ char	*ft_strstr(char *str, char *to_find)
 	int	j;
 
 	i = 0;
+	j = 0;
 	if (to_find[0] == '\0')
 		return (str);
 	while (str[i])
@@ -25,14 +26,12 @@ char	*ft_strstr(char *str, char *to_find)
 		if (str[i] == to_find[0])
 		{
 			j = 0;
-			while (to_find[j] && str[i + j] == to_find[j])
+			while(str[i+j] == to_find[j])
 			{
-				j++;
+			    if(to_find[1+j] == '\0')
+			        return (str+i)l
+			    j++;
 			}
-			if (to_find[j] == '\0')
-				return (str + i);
-		}
-		i++;
 	}
 	return (0);
 }
