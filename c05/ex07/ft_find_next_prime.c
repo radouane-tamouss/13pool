dbd8e6887147a74fb779128dbbd8e78686155db4
Radouane Tamouss <rtamouss@e2r8p5.1337.ma>
2023-09-25 21:23:01 +0100

    hello

--- c05/ex07/ft_find_next_prime.c
+++ c05/ex07/ft_find_next_prime.c
@@ -1,36 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_sqrt.c                                          :+:      :+:    :+:   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rtamouss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/25 16:25:18 by rtamouss          #+#    #+#             */
-/*   Updated: 2023/09/25 18:57:16 by rtamouss         ###   ########.fr       */
+/*   Created: 2023/09/25 20:57:25 by rtamouss          #+#    #+#             */
+/*   Updated: 2023/09/25 21:22:18 by rtamouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	ft_sqrt(int nb)
+int	ft_is_prime(int nb)
 {
-	long	i;
-	long	nbr;
+	int	i;
 
-	nbr = nb;
 	i = 2;
-	if (nbr <= 0)
+	if (nb <= 1)
 		return (0);
-	else if (nbr == 1)
-		return (1);
-	else
+	while (i <= nb / i)
 	{
-		while (i * i <= nbr)
-		{
-			if (i * i == nbr)
-				return (i);
-			i++;
-		}
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	int	i;
+
+	i = nb;
+	if (nb < 2)
+		return (2);
+	while (1)
+	{
+		if (ft_is_prime(i) == 1)
+			return (i);
+		i++;
 	}
 	return (0);
 }
