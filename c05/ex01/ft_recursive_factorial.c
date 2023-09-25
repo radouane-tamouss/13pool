382d3a5274583e8c31fd1e108ed3e17e7f8bb6c1
Radouane Tamouss <rtamouss@e2r12p5.1337.ma>
2023-09-25 16:43:26 +0100

    helo

--- c05/ex01/ft_recursive_factorial.c
+++ c05/ex01/ft_recursive_factorial.c
@@ -6,7 +6,7 @@
 /*   By: rtamouss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 13:32:17 by rtamouss          #+#    #+#             */
-/*   Updated: 2023/09/25 16:39:22 by rtamouss         ###   ########.fr       */
+/*   Updated: 2023/09/25 16:41:03 by rtamouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int	ft_recursive_factorial(int nb)
 {
+	if (nb < 0)
+		return (0);
 	if (nb == 0)
 		return (1);
 	return (nb * ft_recursive_factorial(nb - 1));
