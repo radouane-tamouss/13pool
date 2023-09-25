c9101fc383bbc01edaea1a50d172c6905038a7a9
Radouane Tamouss <rtamouss@e2r8p5.1337.ma>
2023-09-25 21:25:08 +0100

    hello

--- c05/ex06/ft_is_prime.c
+++ c05/ex06/ft_is_prime.c
@@ -6,34 +6,11 @@
 /*   By: rtamouss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 20:29:54 by rtamouss          #+#    #+#             */
-/*   Updated: 2023/09/25 20:56:19 by rtamouss         ###   ########.fr       */
+/*   Updated: 2023/09/25 21:24:57 by rtamouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-
-int	ft_sqrt(int nb)
-{
-	long	i;
-	long	nbr;
-
-	nbr = nb;
-	i = 2;
-	if (nbr <= 0)
-		return (0);
-	else if (nbr == 1)
-		return (1);
-	else
-	{
-		while (i * i <= nbr)
-		{
-			if (i * i == nbr)
-				return (i);
-			i++;
-		}
-	}
-	return (0);
-}
 
 int	ft_is_prime(int nb)
 {
@@ -42,7 +19,7 @@ int	ft_is_prime(int nb)
 	i = 2;
 	if (nb <= 1)
 		return (0);
-	while (i <= ft_sqrt(nb))
+	while (i <= nb / i)
 	{
 		if (nb % i == 0)
 			return (0);
