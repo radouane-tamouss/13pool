9a9e87c12abb40b8459cbb7d43eec0ba901d23b1
Radouane Tamouss <rtamouss@e2r13p4.1337.ma>
2023-09-30 17:44:45 +0100

    hello\

---
+++ c04/ex05/ft_atoi_base.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi_base.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rtamouss <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/30 17:28:47 by rtamouss          #+#    #+#             */
+/*   Updated: 2023/09/30 17:44:29 by rtamouss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+int	is_base_valide(char *base)
+{
+	int	i;
+	int	j;
+
+	if (ft_strlen(base) < 2)
+		return (0);
+	i = 0;
+	while (base[i])
+	{
+		if (base[i] == '-' || base[i] == '+' || base[i] == ' ')
+			return (0);
+		j = i + 1;
+		while (base[j])
+		{
+			if (base[i] == base[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	char_to_int(char c, char *base)
+{
+	int	i;
+
+	i = 0;
+	while (base[i])
+	{
+		if (base[i] == c)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+int	ft_atoi_base(char *str, char *base)
+{
+	int		result;
+	int		sign;
+	int		i;
+	int		base_len;
+
+	result = 0;
+	sign = 1;
+	i = 0;
+	base_len = ft_strlen(base);
+	if (is_base_valide(base) == 0)
+		return (0);
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] && char_to_int(str[i], base) != -1)
+	{
+		result = result * base_len + char_to_int(str[i], base);
+		i++;
+	}
+	return (result * sign);
+}
