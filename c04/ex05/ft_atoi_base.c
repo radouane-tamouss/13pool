50a2de22ea01d5176902684b998186931d47de26
Radouane Tamouss <rtamouss@e2r13p4.1337.ma>
2023-09-30 18:41:33 +0100

    hello

--- c04/ex05/ft_atoi_base.c
+++ c04/ex05/ft_atoi_base.c
@@ -3,127 +3,89 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi_base.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eouhrich <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: rtamouss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/21 09:54:32 by eouhrich          #+#    #+#             */
-/*   Updated: 2023/09/30 16:41:36 by eouhrich         ###   ########.fr       */
+/*   Created: 2023/09/30 17:28:47 by rtamouss          #+#    #+#             */
+/*   Updated: 2023/09/30 18:40:27 by rtamouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include<limits.h>
 
-int	base_is_valide(char *str)
+#include <unistd.h>
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+int	is_base_valide(char *base)
 {
 	int	i;
 	int	j;
 
-	i = 0;
-	if (str[i] == '\0')
+	if (ft_strlen(base) < 2)
 		return (0);
-	while (str[i] != '\0')
+	i = 0;
+	while (base[i])
 	{
-		if (str[i] == '+' || str[i] == '-' || str[i] == 127 || str[i] <= 32)
+		if (base[i] == '-' || base[i] == '+' || base[i] == ' ')
 			return (0);
 		j = i + 1;
-		while (str [j] != '\0')
+		while (base[j])
 		{
-			if (str[i] == str[j])
+			if (base[i] == base[j])
 				return (0);
 			j++;
 		}
 		i++;
 	}
-	if (i == 1)
-		return (0);
 	return (1);
 }
 
-int	exist_in_base(char c, char *base)
+int	char_to_int(char c, char *base)
 {
 	int	i;
 
 	i = 0;
-	while (base[i] != '\0')
+	while (base[i])
 	{
-		if (c == base[i])
-		{
-			return (1);
-		}
+		if (base[i] == c)
+			return (i);
 		i++;
 	}
-	return (0);
-}
-
-int	power(int x, int y)
-{
-	int	i;
-
-	if (y == 0)
-		return (1);
-	i = 1;
-	while (i < y)
-	{
-		x *= x;
-		i++;
-	}
-	return (x);
-}
-
-long	turn_decimal(char *str, int end_index, char *base)
-{
-	long	r;
-	int	i;
-	int	j;
-	int	len_base;
-	len_base = 0;
-	while (base[len_base] != '\0')
-		len_base++;
-	r = 0;
-	i = 0;
-	while (i < end_index)
-	{
-		j = 0;
-		while (base[j] != '\0')
-		{
-			if (str[i] == base[j])
-				break ;
-			j++;
-		}
-		r += j * power(len_base, end_index - 1 - i);
-		i++;
-	}
-	return (r);
+	return (-1);
 }
 
 int	ft_atoi_base(char *str, char *base)
 {
-	int	i;
-	int	sign;
-	int	index_start;
+	int		result;
+	int		sign;
+	int		i;
+	int		base_len;
 
-	if (!base_is_valide(base))
-		return (0);
-	i = 0;
+	result = 0;
 	sign = 1;
+	i = 0;
+	base_len = ft_strlen(base);
+	if (is_base_valide(base) == 0)
+		return (0);
 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
 		i++;
-	while (str[i] == '+' || str[i] == '-')
+	while (str[i] == '-' || str[i] == '+')
 	{
 		if (str[i] == '-')
 			sign *= -1;
 		i++;
 	}
-	index_start = i;
-	while (exist_in_base(str[i], base))
+	while (str[i] && char_to_int(str[i], base) != -1)
+	{
+		result = result * base_len + char_to_int(str[i], base);
 		i++;
-	
-	long x = (sign * turn_decimal(str + index_start, i - index_start, base));
-	if (x == INT_MIN)
-		return (INT_MIN);
-	else
-		return x;
-}
-#include<stdio.h>
-int	main()
-{
-	printf("%d", ft_atoi_base("1111111111111111111111111111111" ,"01"));
+	}
+	return (result * sign);
 }
