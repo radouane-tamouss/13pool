13fed7692fd40c7817d5d33638f1dec3df0d3e1e
Radouane Tamouss <rtamouss@e2r12p7.1337.ma>
2023-10-02 21:03:47 +0100

    hello

--- c09/ex00/ft_strcmp.c
+++ c09/ex00/ft_strcmp.c
@@ -1,45 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rtamouss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/25 00:17:19 by rtamouss          #+#    #+#             */
-/*   Updated: 2023/10/02 14:10:23 by rtamouss         ###   ########.fr       */
+/*   Created: 2023/10/02 14:22:25 by rtamouss          #+#    #+#             */
+/*   Updated: 2023/10/02 17:14:11 by rtamouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-
-void	swap(char **av, int i1, int i2)
-{
-	char	*temp;
-
-	temp = av[i1];
-	av[i1] = av[i2];
-	av[i2] = temp;
-}
-
-void	print(int ac, char **av)
-{
-	int	i;
-	int	j;
-
-	i = 1;
-	j = 0;
-	while (i < ac)
-	{
-		j = 0;
-		while (av[i][j])
-		{
-			write(1, &av[i][j], 1);
-			j++;
-		}
-		write(1, "\n", 1);
-		i++;
-	}
-}
 
 int	ft_strcmp(char *s1, char *s2)
 {
@@ -54,22 +25,4 @@ int	ft_strcmp(char *s1, char *s2)
 			break ;
 	}
 	return (s1[i] - s2[i]);
-}
-
-int	main(int ac, char **av)
-{
-	int	i;
-
-	i = 1;
-	while (i < ac - 1)
-	{
-		if (ft_strcmp(av[i], av[i + 1]) > 0)
-		{
-			swap(av, i, i + 1);
-			i = 0;
-		}
-		i++;
-	}
-	print(ac, av);
-	return (0);
 }
